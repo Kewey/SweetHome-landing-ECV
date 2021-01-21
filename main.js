36f4097bc7cb9af73146f2b84be798a2b0769656
Kewey <jordan-souchez@hotmail.fr>
2021-01-21 13:14:37 +0100

    3d js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-import './assets/styles/main.scss'
+import "./assets/styles/main.scss";
+
+const section = document.querySelector(".bg-perspective");
+const cards = document.querySelector(".pers-card");
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+addEventListener("mousemove", (e) => {
+  const { clientX, clientY } = e;
+
+  let pourcentX = clientX / width - 0.5;
+  let pourcentY = clientY / height - 0.5;
+
+  section.style.transform = `rotateX(${Math.min(
+    30 * pourcentY,
+    12
+  )}deg) rotateY(${Math.min(30 * pourcentX, 12)}deg)`;
+
+  cards.style.transform = `rotateX(${-pourcentY * 30 + 30}deg) rotateY(${
+    pourcentX * 30
+  }deg)`;
+});
