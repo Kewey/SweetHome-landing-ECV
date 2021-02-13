831b5c46583de224870811fe86d1a80a51443699
Kewey <jordan-souchez@hotmail.fr>
2021-02-13 17:06:40 +0100

    finish

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import "./assets/styles/main.scss";
+import './assets/styles/main.scss';
 
 /*
 =============
@@ -7,23 +7,22 @@ import "./assets/styles/main.scss";
 */
 
 const obs = new IntersectionObserver(
-  (obserables) => {
-    obserables.forEach((item) => {
-      if (item.intersectionRatio > 0.5) {
-        item.target.classList.remove("not-visible");
-        obs.unobserve(item.target);
-      }
-    });
-  },
-  {
-    threshold: [0.5],
-  }
+	(items, obs) => {
+		items.forEach((item) => {
+			if (item.intersectionRatio) {
+				item.target.classList.add('slide-up');
+				obs.unobserve(item.target);
+			}
+		});
+	},
+	{
+		rootMargin: '0px 0px -200px 0px'
+	}
 );
 
-let items = document.querySelector(".obs");
+let items = document.querySelectorAll('.obs');
 items.forEach((item) => {
-  item.classList.add("not-visible");
-  obs.observe(item);
+	obs.observe(item);
 });
 
 /*
@@ -32,26 +31,32 @@ items.forEach((item) => {
 =============
 */
 
-const section = document.querySelector(".bg-perspective");
-const cards = document.querySelector(".pers-card");
+const section = document.querySelector('.bg-perspective');
+const cards = document.querySelector('.pers-card');
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-addEventListener("mousemove", (e) => {
-  const { clientX, clientY } = e;
+addEventListener('mousemove', (e) => {
+	const { clientX, clientY } = e;
 
-  let pourcentX = clientX / width - 0.5;
-  let pourcentY = clientY / height - 0.5;
+	let pourcentX = clientX / width - 0.5;
+	let pourcentY = clientY / height - 0.5;
 
-  section.style.transform = `rotateX(${Math.min(
-    30 * pourcentY,
-    12
-  )}deg) rotateY(${Math.min(30 * pourcentX, 12)}deg)`;
+	section.style.transform = `rotateX(${Math.min(
+		-30 * pourcentY,
+		12
+	)}deg) rotateY(${Math.min(30 * pourcentX, 12)}deg)`;
 
-  cards.style.transform = `rotateX(${30 * pourcentY}deg) rotateY(${Math.min(
-    30 * pourcentX,
-    12
-  )}deg)`;
+	cards.style.transform = `rotateX(${-30 * pourcentY}deg) rotateY(${Math.min(
+		30 * pourcentX,
+		12
+	)}deg)`;
+
+	const heroBackground = document.querySelector('.bg-circles');
+	heroBackground.children[1].setAttribute('cx', clientX / 3 + 600);
+	heroBackground.children[2].setAttribute('cx', clientX / 10 + 230);
+	heroBackground.children[3].setAttribute('cx', clientX / 10 + 1920);
+	heroBackground.children[3].setAttribute('cy', clientY / 10 + 200);
 });
 
 /*
@@ -60,16 +65,16 @@ addEventListener("mousemove", (e) => {
 =============
 */
 
-const logoBtn = document.querySelector("#logo");
-const svgLogo = document.querySelector("#shapeLogo");
-const pathLogo = document.querySelector("#Swoome");
+const logoBtn = document.querySelector('#logo');
+const svgLogo = document.querySelector('#shapeLogo');
+const pathLogo = document.querySelector('#Swoome');
 
-logoBtn.addEventListener("mouseenter", (e) => {
-  pathLogo.classList.remove("animLogoPath");
-  svgLogo.classList.remove("animLogoScale");
+logoBtn.addEventListener('mouseenter', (e) => {
+	pathLogo.classList.remove('animLogoPath');
+	svgLogo.classList.remove('animLogoScale');
 
-  setTimeout(() => {
-    pathLogo.classList.add("animLogoPath");
-    svgLogo.classList.add("animLogoScale");
-  }, 10);
+	setTimeout(() => {
+		pathLogo.classList.add('animLogoPath');
+		svgLogo.classList.add('animLogoScale');
+	}, 10);
 });
